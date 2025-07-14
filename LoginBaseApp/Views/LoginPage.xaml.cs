58a450acf7eed87356d5642b84aaf761183cbba5
Owner <stam1@gmail.com>
2025-07-14 10:37:11 +0300

    Added Logic

--- LoginBaseApp/Views/LoginPage.xaml.cs
+++ LoginBaseApp/Views/LoginPage.xaml.cs
@@ -8,13 +8,46 @@ namespace LoginBaseApp.Views;
 public partial class LoginPage : ContentPage
 {
 	DBMokup db;
+	private string _userName;
+	private string _password;
+
+	public string UserName{
+		get => _userName;
+		set
+		{
+			if (_userName != value)
+			{
+				_userName = value;
+				OnPropertyChanged();
+				OnPropertyChanged(nameof(IsEnabled));
+			}
+		}
+		}
+	public string Password
+	{
+		get => _password;
+		set
+		{
+			if (_password != value)
+			{
+				_password = value;
+				OnPropertyChanged();
+				OnPropertyChanged(nameof(IsEnabled));
+			}
+		}
+	}
 	
-	
+	public bool IsEnabled
+	{
+		get => (!string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(Password));
+	}
+
 	public LoginPage()
 	{
 		
 		InitializeComponent();
 		db = new DBMokup();
+		BindingContext = this;
 	}
 
 	private void TogglePasswordVisiblity(object sender, EventArgs e)
